8ed5e3bc0c419d058a301e5fde129b31972ddcce
aaron-dm-mcdonald <mcdonald.dm.aaron@gmail.com>
2026-05-03 17:08:32 -0700

    update script for winners

--- week-8/hw/startup-for-rhel.sh
+++ week-8/hw/startup-for-rhel.sh
@@ -11,7 +11,7 @@ IP=$(curl -H "$HEADER" "$META/network-interfaces/0/ip")
 # have the package manager grab the apache2 webserver 
 dnf install -y httpd
 
-# write our html file to the default location apache2 looks for it 
+# write our html file to the default location apache2 looks for
 cat > /var/www/html/index.html << EOF
 <!DOCTYPE html>
 <html>
@@ -19,6 +19,11 @@ cat > /var/www/html/index.html << EOF
   <h1>VM Metadata</h1>
   <h2>Instance Name: $NAME</h2>
   <h2>Internal IP: $IP</h2>
+  <h2>Colombian prize included for free!</h2>
+  <figure>
+    <img src="https://test-1256099743.s3.us-east-2.amazonaws.com/Colombian/imgi_22_551283556_24677511425231259_7293143846320648055_n.jpg" alt="Colombian prize!" style="max-width:600px; width:100%; display:block; margin:1rem 0;">
+    <figcaption>Colombian prize!</figcaption>
+  </figure>
 </body>
 </html>
 EOF
